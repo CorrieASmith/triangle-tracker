304647d2472d2e51359fb557a4d32196b558c5e7
Corrie Smith & Michael Summers <student@epicodus.com>
2015-09-08 10:25:18 -0700

    completes frontend

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,20 @@ var triangleTracker = function(side1, side2, side3) {
     return "not a triangle";
   };
 };
+
+$(document).ready(function() {
+  $("form#triangle-tracker").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var output = triangleTracker(side1, side2, side3);
+
+    $(".side1").text(side1);
+    $(".side2").text(side2);
+    $(".side3").text(side3);
+    $(".output").text(output);
+
+    $("#output").show();
+    event.preventDefault();
+  });
+});
