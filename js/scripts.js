c4a42abf0d8f70a20519b696d4d80794c28a945f
Corrie Smith & Michael Summers <student@epicodus.com>
2015-09-08 10:02:15 -0700

    Will return scalene or 'not a triangle' options

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ var triangleTracker = function(side1, side2, side3) {
       return "equilateral";
     } else if (side1 === side2 || side2 === side3 || side1 === side3) {
       return "isosceles";
+    } else {
+      return "scalene";
     };
+  } else {
+    return "not a triangle";
   };
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,12 @@ describe('triangleTracker', function () {
   it("returns isosceles for triangles with two equal sides", function () {
     expect(triangleTracker(5, 5, 6)).to.equal("isosceles");
   });
+
+  it("returns scalene for triangles with no equal sides", function () {
+    expect(triangleTracker(5,6,7)).to.equal("scalene");
+  });
+
+  it("returns 'not a triangle' if not a triangle", function () {
+    expect(triangleTracker(5,6, 999)).to.equal("not a triangle");
+  });
 });
