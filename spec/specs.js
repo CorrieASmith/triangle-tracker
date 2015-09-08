ad63ca0577ebb6e6a652bde33bb1e9a7b7b8dc82
Corrie Smith & Michael Summers <student@epicodus.com>
2015-09-08 09:58:52 -0700

    Will return isosceles if two sides are equal

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('triangleTracker', function () {
   it("returns equilateral for triangles with equal sides", function () {
     expect(triangleTracker(5, 5, 5)).to.equal("equilateral");
   });
+
+  it("returns isosceles for triangles with two equal sides", function () {
+    expect(triangleTracker(5, 5, 6)).to.equal("isosceles");
+  });
 });
